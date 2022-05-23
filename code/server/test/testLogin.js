bd41777730631cf29fa47576669db0bcb2d54579
Lorenzo Radaele s301165 <s301165@studenti.polito.it>
2022-05-23 12:42:51 +0200

    Added test Hello word

--- code/server/test/testLogin.js
+++ code/server/test/testLogin.js
@@ -16,11 +16,24 @@ describe("test manage login and logout scenarios", () => {
         await users.deleteAllUsers();
     })
 
+    helloWord();
+
     //SCENARIO 7-1
     login(200, "username@gmail.com", "password99", "clerk");
     //SCENARIO 7-2
     logout(200);
 })
+
+function helloWord() {
+    it('hello', function (done) {
+        agent.get('/api/hello')
+            .then(function (res) {
+                res.should.have.status(200);
+                //res.body.message.should.equal('Hello World!');
+                done();
+            })
+    })
+}
 
 function login(expectedHTTPStatus, username, password, type) {
     it('login', function (done) {
@@ -29,9 +42,11 @@ function login(expectedHTTPStatus, username, password, type) {
             .send(user)
             .then(function (res) {
                 res.should.have.status(201);
-                agent.post('api/' + type + 'Sessions')
-                    .send({ username: username, password: password })
+                let prova = { username: username, password: password };
+                agent.post('api/clerkSessions')
+                    .send(prova)
                     .then(function (rs) {
+                        done();
                         rs.should.have.status(expectedHTTPStatus);
                         agent.get('api/users')
                             .then(function (r) {
@@ -47,8 +62,9 @@ function login(expectedHTTPStatus, username, password, type) {
 }
 
 function logout(expectedHTTPStatus) {
-    it('login', function (done) {
+    it('logout', function (done) {
         let user = { username: username, name: "name", surname: "surname", password: password, type: type }
+        done();
         agent.post('/api/newUser')
             .send(user)
             .then(function (res) {
@@ -64,8 +80,8 @@ function logout(expectedHTTPStatus) {
                                 r.body[0].name.should.equal("name");
                                 r.body[0].email.should.equal(username);
                                 agent.post('api/logout')
-                                    .then(function (r) {
-                                        r.should.have.status(expectedHTTPStatus);
+                                    .then(function (r1) {
+                                        r1.should.have.status(expectedHTTPStatus);
                                         done();
                                     })
                             })
